c2d145f7a16ea50a38683c275bd21f6728f6ee2d
Ondřej Kulhavý <okulhav@gmail.com>
2023-04-20 09:10:45 +0200

    feat: sound effects

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ class Dog {
                 <td>${this.name}</td>
                 <td>${this.maxAge}</td>
                 <td>${this.minAge}</td>
-                <td><img src="${this.img}" alt="dog"></td>
+                <td>${this.img}</td>
             </tr>`;
     }
 }
@@ -32,7 +32,7 @@ function addDog() {
     fetch('https://dogapi.dog/api/v2/breeds')
         .then(response => response.json())
         .then(data => {
-            let random = Math.floor(Math.random(10));
+            let random = Math.floor(Math.random() * 10);
             data['data'][random]
 
             let dog = new Dog(data['data'][random]['attributes']['name'], data['data'][random]['attributes']['life']['min'], data['data'][random]['attributes']['life']['max']);
@@ -40,9 +40,32 @@ function addDog() {
         })
 }
 
+function updateHTML() {
+    let tbody = document.getElementById('obsah');
+    tbody.innerHTML = '';
+    let html = '';
+    for (let i = 0; i < dogs.length; i++) {
+        html += dogs[i].getHTML();
+    }
+    tbody.innerHTML = html;
+}
+
+function loadit() {
+    let range = document.getElementById('range');
+    for (let i = 0; i < range.value; i++) {
+        addDog();
+    }
+    playSound();
+}
+loadit();
+
+setInterval(() => {
+    updateHTML();
+}, 1000);
+
 
 function playSound() {
-    let audio = new Audio('sound.mp3');
+    let audio = new Audio('assets/sound.mp3');
     audio.play();
 }
 
